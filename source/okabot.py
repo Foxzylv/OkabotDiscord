a49089b499276527eedbad855eed9943da69c049
Foxzy <86633618+Foxzylv@users.noreply.github.com>
2022-04-06 17:41:46 -0500

    Update okabot.py

--- source/okabot.py
+++ source/okabot.py
@@ -1,1 +1,20 @@
+import os
+import discord
+my_secret = os.environ['TOKEN']
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if "kongreey" in message.content.lower():
+    await message.channel.send("No! Not Kongreey Kongroo!")
+
+  if "congroo" in message.content.lower():
+    await message.channel.send("No! Not Congroo Kongaroo!")
+
+client.run(os.getenv('TOKEN'))
+my_secret = os.environ['TOKEN']
 
